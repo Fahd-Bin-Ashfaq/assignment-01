79c6b9bc6a1f69f3e60821bc75491bc48c37c329
Fahad Bin Ashfaq <108400515+Fahd-Bin-Ashfaq@users.noreply.github.com>
2024-10-24 15:19:22 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,34 @@
+# a=20 #int
+# print(a)
+# a=a+10
+# print(a)
+# a-=20
+# print(a) #20
+# b=10 #int
+
+# print(a+b)  #30
+# print(a-b)  #10
+# print(a/b)  #2.0
+# print(a*b)  #200
+# print(a//b) #2
+# print(2%2)
+# print(a**b)
+
+# a += 10
+
+# print(a) #30
+
+# male=False
+# print(type(male))
+
+num1=20
+num2=10
+
+result=num1+num2 #30
+result=num1-num2 #10
+result=num1/num2 #2.0
+result=num1*num2 #200
+result=num1//num2 #2
+result=num1%num2
+print(result)
+
